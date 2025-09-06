bbf26cb146a703bde6baf375141dfb89f6c92e1b
swaroopasgit <swaroopasrinivas002@gmail.com>
2025-09-06 01:39:39 -0700

    add

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     version = os.getenv("APP_VERSION", "v0.0.1")
-    return f"Hello from Flask! Version: {version}\n"
+    return f"Hello from ganesha! Version: {version}\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
